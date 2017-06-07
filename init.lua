a77f809f966ff9b570d1636d0a6314042fa5005e
AntumDeluge <antumdeluge@gmail.com>
2017-06-07 01:10:03 -0700

    Replace deprecated method 'setting_getbool' with 'settings:get_bool'

--- init.lua
+++ init.lua
@@ -47,9 +47,9 @@ else
 end
 
 
-drawers.enable_1x1 = not core.setting_getbool("drawers_disable_1x1")
-drawers.enable_1x2 = not core.setting_getbool("drawers_disable_1x2")
-drawers.enable_2x2 = not core.setting_getbool("drawers_disable_2x2")
+drawers.enable_1x1 = not core.settings:get_bool("drawers_disable_1x1")
+drawers.enable_1x2 = not core.settings:get_bool("drawers_disable_1x2")
+drawers.enable_2x2 = not core.settings:get_bool("drawers_disable_2x2")
 
 
 --
