3310c8106e03eebd6ee7a9015fc0fb0027db64eb
isaiah658 <isaiah658@outlook.com>
2018-06-14 19:40:11 -0500

    Change copyright year, comment lengths, and minetest. to core.
    The following changes were requested for a pull request. The copyright year for LNJ was changed to 2017. Some of the longer code comments were turned in to multi-line comments or rewritten slightly to be shorter. All instances of "minetest." were changed to "core." instead.

--- lua/controller.lua
+++ lua/controller.lua
@@ -2,7 +2,7 @@
 Minetest Mod Storage Drawers - A Mod adding storage drawers
 
 Copyright (C) 2018 isaiah658
-Copyright (C) 2018 LNJ <git@lnj.li>
+Copyright (C) 2017 LNJ <git@lnj.li>
 
 MIT License
 
@@ -25,18 +25,28 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ]]--
 
--- The gist of how the drawers mod stores data is that there are entities and the drawer node itself. The entities are needed to allow having multiple drawers in one node. The entities and node each store metadata about the item counts and such. It is necessary to change both at once otherwise in some cases the entity values are used and in other cases the node metadata is used.
+--[[ The gist of how the drawers mod stores data is that there are entities 
+and the drawer node itself. The entities are needed to allow having multiple 
+drawers in one node. The entities and node each store metadata about the item 
+counts and such. It is necessary to change both at once otherwise in some cases 
+the entity values are used and in other cases the node metadata is used.
 
--- The gist of how the controller works is this. The drawer controller scans the adjacent tiles (length and height is configurable) and puts the item names and other info such as coordinates and the visualid of the entity in a table. That table is saved in the controllers metadata. The table is used to help prevent needing to scan all the drawers to deposit an item in certain situations. The table is only updated on an as needed basis, not by a specific time/interval. Controllers that have no items will not continue scanning drawers.
+The gist of how the controller works is this. The drawer controller scans the 
+adjacent tiles (length and height is configurable) and puts the item names and 
+other info such as coordinates and the visualid of the entity in a table. That 
+table is saved in the controllers metadata. The table is used to help prevent 
+needing to scan all the drawers to deposit an item in certain situations. The 
+table is only updated on an as needed basis, not by a specific time/interval. 
+Controllers that have no items will not continue scanning drawers. ]]--
 
 local function controller_can_dig(pos, player)
-	local meta = minetest.get_meta(pos);
+	local meta = core.get_meta(pos);
 	local inv = meta:get_inventory()
 	return inv:is_empty("src")
 end
 
 local function controller_allow_metadata_inventory_put(pos, listname, index, stack, player)
-	if minetest.is_protected(pos, player:get_player_name()) then
+	if core.is_protected(pos, player:get_player_name()) then
 		return 0
 	end
 	if listname == "src" then
@@ -45,14 +55,14 @@ local function controller_allow_metadata_inventory_put(pos, listname, index, sta
 end
 
 local function controller_allow_metadata_inventory_move(pos, from_list, from_index, to_list, to_index, count, player)
-	local meta = minetest.get_meta(pos)
+	local meta = core.get_meta(pos)
 	local inv = meta:get_inventory()
 	local stack = inv:get_stack(from_list, from_index)
 	return controller_allow_metadata_inventory_put(pos, to_list, to_index, stack, player)
 end
 
 local function controller_allow_metadata_inventory_take(pos, listname, index, stack, player)
-	if minetest.is_protected(pos, player:get_player_name()) then
+	if core.is_protected(pos, player:get_player_name()) then
 		return 0
 	end
 	return stack:get_count()
@@ -76,11 +86,23 @@ local function controller_formspec(pos, meta_current_state)
 end
 
 local function index_drawers(pos)
-	-- The pos parameter is the controllers position
+	--[[ The pos parameter is the controllers position
 	
-	-- We store the item name as a string key and the value is a table with position x, position y, position z, and visualid. Those are all strings as well with the values assigned to them that way we don't need to worry about the ordering of the table. The count and max count are not stored as those values have a high potential of being outdated quickly. It's better to grab the values from the drawer when needed so you know you are working with accurate numbers.
+	We store the item name as a string key and the value is a table with position x,
+	position y, position z, and visualid. Those are all strings as well with the 
+	values assigned to them that way we don't need to worry about the ordering of 
+	the table. The count and max count are not stored as those values have a high 
+	potential of being outdated quickly. It's better to grab the values from the 
+	drawer when needed so you know you are working with accurate numbers.
 	
-	-- Indexing starts on the row (meaning same y coordinate) of the controller on the adjacent sides and moves each column on the same y level. A row is searched until it either hits the max length setting size or until a node that isn't a drawer is indexed and then moves up on (y coordinate increases by 1) and starts the process until the y coord is reaches the max height or until a node that isn't a drawer is indexed and at the point it stops indexing all together. This makes it so all drawers need to be next to each other on the rows without spacing or other blocks in between.
+	Indexing starts on the row (meaning same y coordinate) of the controller on the 
+	adjacent sides and moves each column on the same y level. A row is searched until 
+	it either hits the max length setting size or until a node that isn't a drawer is 
+	indexed and then moves up on (y coordinate increases by 1) and starts the process 
+	until the y coord is reaches the max height or until a node that isn't a drawer 
+	is indexed and at the point it stops indexing all together. This makes it so all 
+	drawers need to be next to each other on the rows without spacing or other blocks 
+	in between. ]]--
 	
 	local drawers_table_index = {}
 	local x_or_z_axis = 1
@@ -112,8 +134,8 @@ local function index_drawers(pos)
 				elseif x_z == 4 then
 					drawer_pos = {x = pos.x, y = pos.y + y - 1, z = pos.z - x_or_z}
 				end
-				local drawer_meta = minetest.get_meta(drawer_pos)
-				local drawer_node = minetest.get_node(drawer_pos)
+				local drawer_meta = core.get_meta(drawer_pos)
+				local drawer_node = core.get_node(drawer_pos)
 				
 				-- There might be a better way to know if the node is a drawer other than matching a string in the node name
 				-- Can't trust metadata only in case another mod has a block with the same metadata strings
@@ -133,7 +155,7 @@ local function index_drawers(pos)
 						elseif drawer_meta_name ~= "" and drawer_meta_name ~= nil then
 							-- If we already indexed this item previously, check which drawer has the most space and have that one be the one indexed
 							if drawers_table_index[drawer_meta_name] then
-								local indexed_drawer_meta = minetest.get_meta({x = drawers_table_index[drawer_meta_name]["drawer_pos_x"], y = drawers_table_index[drawer_meta_name]["drawer_pos_y"], z = drawers_table_index[drawer_meta_name]["drawer_pos_z"]})
+								local indexed_drawer_meta = core.get_meta({x = drawers_table_index[drawer_meta_name]["drawer_pos_x"], y = drawers_table_index[drawer_meta_name]["drawer_pos_y"], z = drawers_table_index[drawer_meta_name]["drawer_pos_z"]})
 								local indexed_drawer_meta_count = indexed_drawer_meta:get_int("count" .. drawers_table_index[drawer_meta_name]["visualid"])
 								local indexed_drawer_meta_max_count = indexed_drawer_meta:get_int("max_count" .. drawers_table_index[drawer_meta_name]["visualid"])
 								local drawer_meta_count = drawer_meta:get_int("count" .. visualid)
@@ -156,7 +178,8 @@ local function index_drawers(pos)
 					break
 				end
 			end
-			-- If we ever break out of the above loop while x or z is 1, it means the first block searched in a row did not contain a drawer so we stop searching altogether. If we don't stop on a row that starts with a non drawer node, we have to search all the nodes in the columns adjacent to the controller. That seems wasteful and ineffecient to allow that.
+			-- If we break out of the above loop while x or z is 1, it means the first block searched in a row did not contain a drawer.
+			-- All searching for an axis is stopped when a row starts with a non-drawer.
 			if x_or_z_axis == 1 then
 				break
 			end
@@ -168,7 +191,7 @@ end
 
 local function controller_node_timer(pos, elapsed)
 	-- Inizialize metadata
-	local meta = minetest.get_meta(pos)
+	local meta = core.get_meta(pos)
 	local meta_current_state = meta:get_string("current_state")
 	local meta_times_ran_while_jammed = meta:get_float("times_ran_while_jammed")
 	local meta_jammed_item_name = meta:get_string("jammed_item_name")
@@ -176,11 +199,23 @@ local function controller_node_timer(pos, elapsed)
 	local src = inv:get_stack("src", 1)
 	local src_name = src:get_name()
 	
-	-- There are four scenarios for the item slot in the controller. 1: No item is in the controller. 2: Item is not stackable. 3. Item is allowed and there is either an existing drawer for that item with room or an empty drawer. 4: Item is allowed, but there is no room.
+	--[[ There are four scenarios for the item slot in the controller. 
+	1: No item is in the controller. 
+	2: Item is not stackable. 
+	3. Item is allowed and there is either an existing drawer for that item with room or an empty drawer. 
+	4: Item is allowed, but there is no room.
 	
-	-- There are three different possibilities for "current_state". 1: "running" which means means it's operating normally. 2: "stopped" meaning the controller makes no attempt to put in the item possibly due to being unallowed for various reasons. 3: "jammed" meaning the item is allowed in to drawers, but there was no space to deposit it last time it ran.
+	There are three different possibilities for "current_state". 
+	1: "running" which means means it's operating normally. 
+	2: "stopped" meaning the controller makes no attempt to put in the item possibly due to being unallowed for various reasons. 
+	3: "jammed" meaning the item is allowed in to drawers, but there was no space to deposit it last time it ran. ]]--
 	
-	-- If current state is jammed, the item that jammed it is the same item in the src inv slot, and the amount of times ran while jammed is 8 or higher, we set the current state to stopped (will possibly want to make an option in the formspec to ignore this an continue running if the user plans on using the system in a way that may cause frequent jams making it a hassle to manually clear it each time)
+	--[[ If current state is jammed, the item that jammed it is the same item in the 
+	src inv slot, and the amount of times ran while jammed is 8 or higher, we 
+	set the current state to stopped. Will possibly want to make an option in the 
+	formspec to ignore this an continue running if the user plans on using the 
+	system in a way that may cause frequent jams making it a hassle to manually 
+	clear it each time ]]--
 	if meta_current_state == "jammed" and meta_jammed_item_name == src_name and meta_times_ran_while_jammed >= 8 then
 		meta:set_string("current_state", "stopped")
 		meta:set_string("formspec", controller_formspec(pos, "Stopped"))
@@ -217,25 +252,25 @@ local function controller_node_timer(pos, elapsed)
 	end
 	
 	-- If the index has not been created, the item isn't in the index, the item in the drawer is no longer the same item in the index, or the item is in the index but it's full, run the index_drawers function
-	local drawers_table_index = minetest.deserialize(meta:get_string("drawers_table_index"))
+	local drawers_table_index = core.deserialize(meta:get_string("drawers_table_index"))
 	-- If the index has not been created
 	if not drawers_table_index then
 		drawers_table_index = index_drawers(pos)
-		meta:set_string("drawers_table_index", minetest.serialize(drawers_table_index))
+		meta:set_string("drawers_table_index", core.serialize(drawers_table_index))
 	-- If the item isn't in the index
 	elseif not drawers_table_index[src_name] then
 		drawers_table_index = index_drawers(pos)
-		meta:set_string("drawers_table_index", minetest.serialize(drawers_table_index))
+		meta:set_string("drawers_table_index", core.serialize(drawers_table_index))
 	-- If the item is in the index but either the name that was indexed is not the same as what is currently in the drawer or the drawer is full
 	elseif drawers_table_index[src_name] then
 		local visualid = drawers_table_index[src_name]["visualid"]
-		local indexed_drawer_meta = minetest.get_meta({x = drawers_table_index[src_name]["drawer_pos_x"], y = drawers_table_index[src_name]["drawer_pos_y"], z = drawers_table_index[src_name]["drawer_pos_z"]})
+		local indexed_drawer_meta = core.get_meta({x = drawers_table_index[src_name]["drawer_pos_x"], y = drawers_table_index[src_name]["drawer_pos_y"], z = drawers_table_index[src_name]["drawer_pos_z"]})
 		local indexed_drawer_meta_name = indexed_drawer_meta:get_string("name" .. visualid)
 		local indexed_drawer_meta_count = indexed_drawer_meta:get_int("count" .. visualid)
 		local indexed_drawer_meta_max_count = indexed_drawer_meta:get_int("max_count" .. visualid)
 		if indexed_drawer_meta_name ~= src_name or indexed_drawer_meta_count >= indexed_drawer_meta_max_count then
 			drawers_table_index = index_drawers(pos)
-			meta:set_string("drawers_table_index", minetest.serialize(drawers_table_index))
+			meta:set_string("drawers_table_index", core.serialize(drawers_table_index))
 		end
 	end
 	
@@ -251,12 +286,12 @@ local function controller_node_timer(pos, elapsed)
 	if drawers_table_index[src_name] then
 		local indexed_drawer_pos = {x = drawers_table_index[src_name]["drawer_pos_x"], y = drawers_table_index[src_name]["drawer_pos_y"], z = drawers_table_index[src_name]["drawer_pos_z"]}
 		local visualid = drawers_table_index[src_name]["visualid"]
-		local indexed_drawer_meta = minetest.get_meta(indexed_drawer_pos)
+		local indexed_drawer_meta = core.get_meta(indexed_drawer_pos)
 		local indexed_drawer_meta_name = indexed_drawer_meta:get_string("name" .. visualid)
 		local indexed_drawer_meta_count = indexed_drawer_meta:get_int("count" .. visualid)
 		local indexed_drawer_meta_max_count = indexed_drawer_meta:get_int("max_count" .. visualid)
 		-- If the the item in the drawer is the same as the one we are trying to store, the drawer is not full, and the drawer entity is loaded, we will put the items in the drawer
-		if indexed_drawer_meta_name == src_name and indexed_drawer_meta_count < indexed_drawer_meta_max_count and drawers.drawer_visuals[minetest.serialize(indexed_drawer_pos)] then
+		if indexed_drawer_meta_name == src_name and indexed_drawer_meta_count < indexed_drawer_meta_max_count and drawers.drawer_visuals[core.serialize(indexed_drawer_pos)] then
 			local leftover = drawers.drawer_insert_object(indexed_drawer_pos, nil, src, nil)
 			inv:set_stack("src", 1, leftover)
 			-- Set the controller metadata
@@ -272,10 +307,10 @@ local function controller_node_timer(pos, elapsed)
 	elseif drawers_table_index["empty"] then
 		local indexed_drawer_pos = {x = drawers_table_index["empty"]["drawer_pos_x"], y = drawers_table_index["empty"]["drawer_pos_y"], z = drawers_table_index["empty"]["drawer_pos_z"]}
 		local visualid = drawers_table_index["empty"]["visualid"]
-		local indexed_drawer_meta = minetest.get_meta(indexed_drawer_pos)
+		local indexed_drawer_meta = core.get_meta(indexed_drawer_pos)
 		local indexed_drawer_meta_name = indexed_drawer_meta:get_string("name" .. visualid)
 		-- If the drawer is still empty and the drawer entity is loaded, we will put the items in the drawer
-		if indexed_drawer_meta_name == "" and drawers.drawer_visuals[minetest.serialize(indexed_drawer_pos)] then
+		if indexed_drawer_meta_name == "" and drawers.drawer_visuals[core.serialize(indexed_drawer_pos)] then
 			local leftover = drawers.drawer_insert_object(indexed_drawer_pos, nil, src, nil)
 			inv:set_stack("src", 1, leftover)
 			-- Add the item to the drawers table index and set the empty one to nil
@@ -285,7 +320,7 @@ local function controller_node_timer(pos, elapsed)
 			meta:set_string("current_state", "running")
 			meta:set_string("formspec", controller_formspec(pos, "Running"))
 			meta:set_float("times_ran_while_jammed", 0)
-			meta:set_string("drawers_table_index", minetest.serialize(drawers_table_index))
+			meta:set_string("drawers_table_index", core.serialize(drawers_table_index))
 		else
 			meta:set_string("current_state", "jammed")
 			meta:set_string("formspec", controller_formspec(pos, "Jammed"))
@@ -309,7 +344,7 @@ controller_def.tiles = {"drawer_controller_top_bottom.png", "drawer_controller_t
 controller_def.can_dig = controller_can_dig
 controller_def.groups = {cracky = 3, level = 2}
 controller_def.on_construct = function(pos)
-	local meta = minetest.get_meta(pos)
+	local meta = core.get_meta(pos)
 	local inv = meta:get_inventory()
 	inv:set_size('src', 1)
 	meta:set_string("current_state", "running")
@@ -317,14 +352,14 @@ controller_def.on_construct = function(pos)
 	meta:set_string("jammed_item_name", "")
 	meta:set_string("drawers_table_index", "")
 	meta:set_string("formspec", controller_formspec(pos, "Running"))
-	local timer = minetest.get_node_timer(pos)
+	local timer = core.get_node_timer(pos)
 	timer:start(7)
 end
 controller_def.on_blast = function(pos)
 	local drops = {}
 	default.get_inventory_drops(pos, "src", drops)
 	drops[#drops+1] = "drawers:controller"
-	minetest.remove_node(pos)
+	core.remove_node(pos)
 	return drops
 end
 controller_def.on_timer = controller_node_timer
@@ -333,17 +368,17 @@ controller_def.allow_metadata_inventory_move = controller_allow_metadata_invento
 controller_def.allow_metadata_inventory_take = controller_allow_metadata_inventory_take
 
 -- Mostly copied from the drawers in the drawer mod to add pipeworks support
-if minetest.get_modpath("pipeworks") and pipeworks then
+if core.get_modpath("pipeworks") and pipeworks then
 	controller_def.groups.tubedevice = 1
 	controller_def.groups.tubedevice_receiver = 1
 	controller_def.tube = controller_def.tube or {}
 	controller_def.tube.insert_object = function(pos, node, stack, tubedir)
-		local meta = minetest.get_meta(pos)
+		local meta = core.get_meta(pos)
 		local inv = meta:get_inventory()
 		return inv:add_item("src", stack)
 	end
 	controller_def.tube.can_insert = function(pos, node, stack, tubedir)
-		local meta = minetest.get_meta(pos)
+		local meta = core.get_meta(pos)
 		local inv = meta:get_inventory()
 		return inv:room_for_item("src", stack)
 	end
@@ -353,11 +388,11 @@ if minetest.get_modpath("pipeworks") and pipeworks then
 	controller_def.after_dig_node = pipeworks.after_dig
 end
 
-minetest.register_node('drawers:controller', controller_def)
+core.register_node('drawers:controller', controller_def)
 
 -- Because the rest of the drawers mod doesn't have a hard depend on default, I changed the recipe to have an alternative
-if minetest.get_modpath("default") and default then
-	minetest.register_craft({
+if core.get_modpath("default") and default then
+	core.register_craft({
 		output = 'drawers:controller',
 		recipe = {
 			{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
@@ -366,7 +401,7 @@ if minetest.get_modpath("default") and default then
 		}
 	})
 else
-	minetest.register_craft({
+	core.register_craft({
 		output = 'drawers:controller',
 		recipe = {
 			{'group:stone', 'group:stone', 'group:stone'},
