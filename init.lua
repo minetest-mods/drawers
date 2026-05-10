8e3bb6de710b12bbb88ed1077006514660036b06
The4spaceconsts <the3spaceconstants@gmail.com>
2026-05-10 16:38:11 -0500

    stuff

--- init.lua
+++ init.lua
@@ -26,8 +26,7 @@ SOFTWARE.
 
 local MP = core.get_modpath(core.get_current_modname())
 
-local S = core.get_translator('drawers')
-local NS = core.get_translator('drawers')
+local S, NS = core.get_translator('drawers')
 
 drawers = {}
 drawers.drawer_visuals = {}
